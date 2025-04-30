ca4d89e0322e3dd2a07608f5945552c2222b042a
khakanyiba <4202375@myuwc.ac.za>
2025-04-30 22:00:45 +0200

    Moved frontend files to root for deployment

--- guideian/guideian/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import User, Course
-
-# Register the custom User model with the UserAdmin
-admin.site.register(User, UserAdmin)
-
-# Register the Course model
-admin.site.register(Course)
